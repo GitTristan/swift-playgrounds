ca38b121ca6facb4e7f3dd4d5dc67bc438213df6
Tristan Rubadeau <tristan@rubadeau.com>
2015-09-05 11:15:59 -0600

    playgrounds initial

---
+++ Strings.playground/Contents.swift
@@ -0,0 +1,5 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+println("List:\n1\t🙀\n2\tBananas\n3\tGrapes")
---
+++ numbers.playground/Contents.swift
@@ -0,0 +1,13 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+
+let language = "Swift"
+
+let year = 2014
+
+var version = 1.0
+
+var isAwesome = true
+
